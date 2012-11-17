4498ff45de47b48dc3584b91875ae5cdc29a3d16
Henry Zhu <hz@therealhenry.com>
2012-11-17 03:49:53 -0800

    added strikethrough

--- todo.js
+++ todo.js
@@ -1,14 +1,21 @@
-          $('form').submit(function(){
-            if ($('input').val() !== '') {
-              var input_value = $('input').val();
-              $('ul').append('<li><a href=#>x</a>' + input_value + '</li>');
-            };
-              $('input').val(''); // clears text input after submit!
-              return false;
-              // prevent default
-          });
-          
-            $(document).on('click', 'a', function (e) {
-              e.preventDefault(); // same as 'return false'?
-              $(this).parent().remove();
-            });
+$('form').submit(function(){
+  if ($('input').val() !== '' ) {
+    var input_value = $('input').val();
+    $('ul').append('<li>' + '<div id="check"></div>' + input_value + '<a href="#">X</a>' + '</li>');
+  };
+
+  $('input').val(''); // clears text input after submit
+  return false; // prevent default?
+});
+
+$(document).on('click', 'a', function (e) {
+  e.preventDefault();
+  $(this).parent().remove();
+});
+
+$(document).on('click', '#check', function (e) {
+	e.preventDefault();
+    $(this).parent().css('text-decoration', 'line-through'),
+    $('a').css('text-decoration', 'none'); // doesn't work 
+});
+
